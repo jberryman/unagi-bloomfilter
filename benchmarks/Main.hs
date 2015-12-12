becd119b3438bb52eab97490ced2c31c44aeabce
Brandon Simmons <brandon.m.simmons@gmail.com>
2015-12-11 22:57:10 -0500

    new microbench

--- benchmarks/Main.hs
+++ benchmarks/Main.hs
@@ -13,8 +13,10 @@ import Data.Hashabler
 main :: IO ()
 main = do
     b_5_20 <- Bloom.new (SipKey 1 1) 5 20
+    b_13_20 <- Bloom.new (SipKey 1 1) 13 20 -- needs 128
     defaultMain [
       bgroup "internals" [
-        bench "membershipWordAndBits64" $ nf (membershipWordAndBits64 (Hash64 0)) b_5_20
+          bench "membershipWordAndBits64" $ nf (membershipWordAndBits64 (Hash64 1)) b_5_20
+        , bench "membershipWordAndBits128" $ nf (membershipWordAndBits128 (Hash128 1 1)) b_13_20
         ]
       ]
